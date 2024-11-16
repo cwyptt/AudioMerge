7e401e793437305a022ef04f42816f315657c6c0
cwyptt <cwyptt@gmail.com>
2024-11-15 18:58:50 -0600

    init

--- resources/AudioMergeMKV_Release/audiomergemkv-onefile-packager.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-pyinstaller --onefile --name "AudioMergeMKV" AudioMergeMKV_Release.py --icon="../AudioMergeMKV.ico" --add-binary="../../ffmpeg/bin/ffmpeg.exe;ffmpeg/bin" --add-binary="../python312.dll;." --add-data="../../presets;presets" --hidden-import "tkinter" --noconsole
-
--- resources/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cd ..
-pyinstaller AudioMergeMKV.spec
